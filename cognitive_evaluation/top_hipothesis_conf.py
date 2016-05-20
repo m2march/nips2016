501006555e83daf330b0716b4c69c83d93379e23
March <m2.march@gmail.com>
2016-05-19 23:08:24 -0300

    Better looking plots

--- cognitive_evaluation/top_hipothesis_conf.py
+++ cognitive_evaluation/top_hipothesis_conf.py
@@ -5,6 +5,8 @@ import glob
 import plotting.onsets as po
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import numpy as np
+import collections
 
 from tht import midi
 from tht.tht import tactus_hypothesis_tracker
@@ -22,13 +24,25 @@ tracker = tactus_hypothesis_tracker.TactusHypothesisTracker(
     max_hypotheses=defaults.max_hypotheses)
 
 
+examples = collections.OrderedDict([
+    ['Sudden tempo change', 'examples/duration_step_change.mid'],
+    ['Slow tempo change', 'examples/slow_tempo_change.mid'],
+    ['Syncopation', 'examples/syncopation.mid'],
+    ['Varying musical patterns', 'examples/varying_musical_patterns.mid']
+])    
+
 def analyze_examples(analyzer):
-    for example_file in glob.glob('examples/*.mid'):
+    analysis = {}
+    for title, example_file in examples.items():
         onsets = midi.midi_to_collapsed_onset_times(example_file) 
         hts = tracker(onsets)
         hts_at_time = tracking_overtime.OvertimeTracking(hts)
-
-        analyzer(os.path.basename(example_file), hts, hts_at_time)
+        analysis[title] = {
+            'hts': hts,
+            'hat': hts_at_time,
+            'file_name': os.path.basename(example_file)
+        }
+    return analysis
 
 
 def calculate_hypothesis_changes(hat_sorted_by_conf):
@@ -73,53 +87,51 @@ def print_conf_per_hypothesis(hts):
                 ['%.2f' % c for c in cs])
 
 
-def output_hipothesis_conf_eval(file_name, hts, hts_at_time):
+def plot_hipothesis_conf_eval(ax, file_name, hts, hts_at_time):
     hypothesis_count = [len(hats)
                         for time, hats in hts_at_time.hypothesis_by_time()]
 
     hat_sorted_by_conf = list(hts_at_time.hypothesis_sorted_by_conf())
-    times, rel_top_conf = zip(*[
-        (time, hats[0].conf / sum([x.conf for x in hats]))
-        for time, hats in hat_sorted_by_conf])
-    top_conf = [hats[0].conf for _, hats in hat_sorted_by_conf]
+
+    times, top_conf = zip(*[
+        (time, hats[0].conf)
+        for time, hats in hat_sorted_by_conf
+    ])
 
     changes = calculate_hypothesis_changes(hat_sorted_by_conf)
+    c_times, c_conf  = zip(*[
+        (c.hts.onset_times[c.onset_idx], c.conf)
+        for c in changes
+    ])
 
     print_hypothesis_evolution(file_name, times, hypothesis_count, changes)
 
-    f = plt.figure()
-    gs = gridspec.GridSpec(2, 1, height_ratios=(2, 1))
-    ax1 = plt.subplot(gs[0])
-    ax1.plot(times, top_conf, color='green')
-    ax1.set_ylabel('top confidence value')
-    ax1.set_ylim(0, 1.1)
-    ax1.set_xlim(0, times[-1]) 
-    ax1.set_frame_on(False)
-    plt.tick_params(bottom=False, left=False, top=False, right=False)
+    ax.plot(times, top_conf, color='forestgreen', lw=1.5)
+    ax.plot(c_times, c_conf, ls=' ', marker='|', ms=12, markeredgewidth=2,
+            color='darkred')
+    ax.set_ylim(0, 1.1)
+    ax.set_xlim(0, times[-1]) 
+    ax.set_yticks([0, 1])
+    ax.set_yticks(np.arange(0, 1, 0.2), minor=True)
+    ax.set_frame_on(False)
+    plt.tick_params(bottom=False, left=False, top=False, right=False,
+            labelbottom=False)
     plt.grid(which='both', lw=1, ls=':', color='grey', alpha=0.6)
 
-
-    for idx, c in enumerate(changes):
-        x = c.hts.onset_times[c.onset_idx]
-        ax1.vlines(x=x, ymin=0, ymax=0.5, color='black', alpha=0.7)
-        y = 0.4 + (.075 * (idx % 3))
-        ax1.text(x=x + 10, y=y  , s=str(c.hts), va='center')
-
-    ax2 = plt.subplot(gs[1])
-    po.prepare_for_onsets(ax2)
-    ax2.set_ylim(0, 1)
-    ax2.vlines(x=hts_at_time.onset_times, ymin=0, ymax=0.7)
-    for idx, time in enumerate(hts_at_time.onset_times):
-        y = 0.75 + 0.05 * (idx % 2)
-        ax2.text(time, y, str(idx), size=6, ha='center')
-
-    plt.xlabel('Onset time (ms)')
-    plt.savefig(os.path.join('visualizations', 
-        file_name.replace('.mid', '_top_conf.pdf')))
+    ax.vlines(x=hts_at_time.onset_times, ymin=0, ymax=0.3, lw=2)
 
 
 def main():
-    analyze_examples(output_hipothesis_conf_eval)
+    analysis = analyze_examples(plot_hipothesis_conf_eval)
+    for idx, (title, d) in enumerate(analysis.items()):
+        plt.figure(figsize=(4,2))
+        hts = d['hts']
+        hat = d['hat']
+        file_name = d['file_name'].replace('.mid', '_top_conf.pdf')
+        ax = plt.gca()
+        plot_hipothesis_conf_eval(ax, title, hts, hat)
+        plt.tight_layout()
+        plt.savefig('visualizations/' + file_name)
 
 
 if __name__ == '__main__':
